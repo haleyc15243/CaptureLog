663d667ed1fc3ccb8a14050abb317bd73ef7fee6
haleyc15243 <richard.cunningham@maine.edu>
2025-12-10 14:42:53 -0500

    Update target SDK.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -23,6 +23,7 @@ kotlin {
     sourceSets.all {
         languageSettings {
             optIn("kotlin.time.ExperimentalTime")
+            optIn("UNIVERSAL_RESIZABLE_BY_DEFAULT")
         }
     }
 
