f3b661beef6a7009d95c8eb401a8dc1dc2666653
haleyc15243 <richard.cunningham@maine.edu>
2025-12-22 19:27:56 -0500

    Move nav graph and CaptureLogScaffold into their own files.

--- composeApp/src/commonMain/kotlin/com/rhc/capturelog/App.kt
+++ composeApp/src/commonMain/kotlin/com/rhc/capturelog/App.kt
@@ -1,12 +1,33 @@
 package com.rhc.capturelog
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.rhc.capturelog.core.navigation.CaptureLogScaffold
+import com.rhc.capturelog.core.navigation.Destination
+import com.rhc.capturelog.features.dailynotes.DailyNotesScreen
 import com.rhc.capturelog.ui.theme.CaptureLogTheme
 
 @Composable
 fun App() {
     CaptureLogTheme {
-        CaptureLogScaffold()
+        val navController = rememberNavController()
+        CaptureLogScaffold(navController) { paddingValues ->
+            NavHost(
+                navController = navController,
+                startDestination = Destination.Today  // Direct start
+            ) {
+                composable<Destination.Today> {
+                    DailyNotesScreen(paddingValues)
+                }
+                composable<Destination.Queue> {
+                    // QueueScreen
+                }
+                composable<Destination.Settings> {
+                    // SettingsScreen
+                }
+            }
+        }
     }
 }
