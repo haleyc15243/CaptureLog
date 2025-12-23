6ce692b006ade08fa9182c6406214e1a76db41ee
haleyc15243 <richard.cunningham@maine.edu>
2025-12-23 06:19:52 -0500

    Fix expect/actual contract.

--- composeApp/src/androidMain/kotlin/com/rhc/capturelog/database/DriverFactory.kt
+++ composeApp/src/androidMain/kotlin/com/rhc/capturelog/database/DriverFactory.kt
@@ -3,9 +3,10 @@ package com.rhc.capturelog.database
 import android.content.Context
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import coil3.PlatformContext
 import com.rhc.capturelog.AppDatabase
 
-actual class DatabaseDriverFactory(val context: Context) {
+actual class DatabaseDriverFactory actual constructor(val context: PlatformContext) {
     actual fun createDriver(): SqlDriver {
         return AndroidSqliteDriver(AppDatabase.Schema, context, "app.db")
     }
--- composeApp/src/commonMain/kotlin/com/rhc/capturelog/database/DriverFactory.kt
+++ composeApp/src/commonMain/kotlin/com/rhc/capturelog/database/DriverFactory.kt
@@ -1,7 +1,8 @@
 package com.rhc.capturelog.database
 
 import app.cash.sqldelight.db.SqlDriver
+import coil3.PlatformContext
 
-expect class DatabaseDriverFactory {
+expect class DatabaseDriverFactory(context: PlatformContext) {
     fun createDriver(): SqlDriver
 }
