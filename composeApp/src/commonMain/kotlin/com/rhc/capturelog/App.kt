6a803301224523c0edeeb3bd1e7c6db791524417
haleyc15243 <richard.cunningham@maine.edu>
2025-12-22 20:11:39 -0500

    Remove duplicate NavHost.

--- composeApp/src/commonMain/kotlin/com/rhc/capturelog/App.kt
+++ composeApp/src/commonMain/kotlin/com/rhc/capturelog/App.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.rhc.capturelog.core.navigation.CaptureLogNavigation
 import com.rhc.capturelog.core.navigation.CaptureLogScaffold
 import com.rhc.capturelog.core.navigation.Destination
 import com.rhc.capturelog.features.dailynotes.DailyNotesScreen
@@ -14,20 +15,7 @@ fun App() {
     CaptureLogTheme {
         val navController = rememberNavController()
         CaptureLogScaffold(navController) { paddingValues ->
-            NavHost(
-                navController = navController,
-                startDestination = Destination.Today  // Direct start
-            ) {
-                composable<Destination.Today> {
-                    DailyNotesScreen(paddingValues)
-                }
-                composable<Destination.Queue> {
-                    // QueueScreen
-                }
-                composable<Destination.Settings> {
-                    // SettingsScreen
-                }
-            }
+            CaptureLogNavigation(paddingValues, navController)
         }
     }
 }
