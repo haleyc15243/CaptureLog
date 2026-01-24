261e60edf0d29eb99b6fe856b88900db4ca1b639
haleyc15243 <richard.cunningham@maine.edu>
2026-01-24 07:37:48 -0500

    Provide the IO dispatcher to the capture data source.

--- composeApp/src/commonMain/kotlin/com/rhc/capturelog/database/DatabaseModule.kt
+++ composeApp/src/commonMain/kotlin/com/rhc/capturelog/database/DatabaseModule.kt
@@ -1,11 +1,15 @@
 package com.rhc.capturelog.database
 
-import com.rhc.capturelog.database.dailyNote.DailyNoteDataSource
+import com.rhc.capturelog.core.dispatchers.IODispatcher
+import com.rhc.capturelog.database.capture.CaptureDataSource
+import kotlinx.coroutines.CoroutineDispatcher
+import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Provided
 import org.koin.core.annotation.Single
 
 @Module
+@ComponentScan("com.rhc.capturelog.database")
 class DatabaseModule {
     @Single
     fun provideDatabaseFactory(
@@ -15,6 +19,7 @@ class DatabaseModule {
 
     @Single
     fun provideDailyNoteDataSource(
+        @IODispatcher ioDispatcher: CoroutineDispatcher,
         databaseFactory: DatabaseFactory
-    ): DailyNoteDataSource = databaseFactory.dailyNoteDataSource()
+    ): CaptureDataSource = databaseFactory.dailyNoteDataSource(ioDispatcher)
 }
